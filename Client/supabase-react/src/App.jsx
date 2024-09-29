e56324cd83e0945de8d16f733f814fb001b91355
Anurag <anurag3487@gmail.com>
2024-09-29 23:00:24 +0530

    hooking up Login page wrt Supabase

--- Client/supabase-react/src/App.jsx
+++ Client/supabase-react/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Login from "./components/login";
 import Success from "./components/success";
 import Home from "./components/home";
--- Client/supabase-react/src/components/login.jsx
+++ Client/supabase-react/src/components/login.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useNavigate } from "react-router-dom";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabasePublicKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabasePublicKey);
+
 const Login = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange(async (event) => {
+      console.log(event);
+      if (event === "SIGNED_IN") {
+        navigate("/success");
+      } else {
+        navigate("/login");
+      }
+    });
+  }, []);
+
   return (
     <>
-      <h1>Login</h1>
+      <div className="container">
+        <h1 className="center">Login</h1>
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          providers={["github"]}
+          theme="dark"
+        />
+      </div>
     </>
   );
 };
