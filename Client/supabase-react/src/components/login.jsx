5a551047226d687c7f1c879cd7c2a2b6142d082d
Anurag <anurag3487@gmail.com>
2024-09-30 00:00:14 +0530

    hookup logout from success page

--- Client/supabase-react/src/components/login.jsx
+++ Client/supabase-react/src/components/login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
@@ -9,18 +9,21 @@ const supabasePublicKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabasePublicKey);
 
 const Login = () => {
+  const [event, setEvent] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     supabase.auth.onAuthStateChange(async (event) => {
       console.log(event);
       if (event === "SIGNED_IN") {
+        setEvent(event);
         navigate("/success");
       } else {
+        setEvent(event);
         navigate("/login");
       }
     });
-  }, []);
+  }, [event]);
 
   return (
     <>
--- Client/supabase-react/src/components/success.jsx
+++ Client/supabase-react/src/components/success.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { useNavigate } from "react-router-dom";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabasePublicKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabasePublicKey);
+
 const Success = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState("");
+  console.log("User data", user);
+
+  useEffect(() => {
+    async function userData() {
+      await supabase.auth.getUser().then((x) => {
+        if (x.data?.user) {
+          setUser(x.data?.user);
+        }
+      });
+    }
+    userData();
+  }, []);
+
+  async function logOut() {
+    await supabase.auth.signOut();
+    navigate("/login");
+  }
   return (
     <>
-      <h1>Success</h1>
+      {Object.keys(user).length != 0 ? (
+        <>
+          <h1>Success</h1>
+          <button onClick={logOut}>LogOut</button>
+        </>
+      ) : (
+        <>
+          <h1>user is not logged in</h1>
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Go back to home
+          </button>
+        </>
+      )}
     </>
   );
 };
