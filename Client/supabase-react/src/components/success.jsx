b8b7b6a5b610db1eca5e73a5158ccd5333bd53d2
Anurag <anurag3487@gmail.com>
2024-09-30 22:17:23 +0530

    call supabase JWT protected API from .NET end

--- Client/supabase-react/src/components/success.jsx
+++ Client/supabase-react/src/components/success.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabasePublicKey = import.meta.env.VITE_SUPABASE_KEY;
@@ -10,6 +11,7 @@ const Success = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState("");
   const [token, setToken] = useState("");
+  const [emp, setEmp] = useState({});
 
   useEffect(() => {
     async function userData() {
@@ -32,12 +34,29 @@ const Success = () => {
     await supabase.auth.signOut();
     navigate("/login");
   }
+
+  async function callAPI() {
+    const API_URL = "https://localhost:7000/api/Values/names";
+    let config = {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+    const response = await axios.get(API_URL, config);
+    setEmp(response.data);
+  }
   return (
     <>
       {Object.keys(user).length != 0 ? (
         <>
           <h1>Success</h1>
           <button onClick={logOut}>LogOut</button>
+          <button onClick={callAPI}>Call API</button>
+          <div className="empContainer">
+            <p>{emp.id}</p>
+            <p>{emp.name}</p>
+            <p>{emp.department}</p>
+          </div>
         </>
       ) : (
         <>
