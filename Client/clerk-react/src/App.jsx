9a0a394d08bcd7ffd86d5e7c91754cddd65d96fe
Anurag <anurag3487@gmail.com>
2024-10-01 16:35:51 +0530

    add Clerk email provider auth in React FE

--- Client/clerk-react/src/App.jsx
+++ Client/clerk-react/src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
       <h1>Clerk Auth</h1>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
   );
 }
