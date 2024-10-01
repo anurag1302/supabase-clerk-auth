a800b3470f102cb68df6a6f9710b12e7a07589af
Anurag <anurag3487@gmail.com>
2024-10-01 23:01:09 +0530

    Call .NET protected APIs with CLerk JWT

--- Client/clerk-react/src/App.jsx
+++ Client/clerk-react/src/App.jsx
@@ -6,6 +6,7 @@ import {
   UserButton,
 } from "@clerk/clerk-react";
 
+import Success from "./components/success";
 function App() {
   return (
     <>
@@ -16,6 +17,7 @@ function App() {
         </SignedOut>
         <SignedIn>
           <UserButton />
+          <Success />
         </SignedIn>
       </header>
     </>
