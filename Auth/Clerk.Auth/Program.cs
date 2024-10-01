36e958250abf36829f7baf3c3712b65c71e25153
Anurag <anurag3487@gmail.com>
2024-10-01 23:02:23 +0530

    Validate Clerk JWT auth and protect APis

--- Auth/Clerk.Auth/Program.cs
+++ Auth/Clerk.Auth/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace Clerk.Auth
 {
     public class Program
@@ -6,6 +10,7 @@ namespace Clerk.Auth
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
 
             // Add services to the container.
 
@@ -13,6 +18,44 @@ namespace Clerk.Auth
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = configuration["ClerkSettings:Authority"];
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["ClerkSettings:SecretKey"]))
+                    };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = context =>
+                        {
+                            var principal = context.Principal;
+                            var azpClaim = principal?.Claims.FirstOrDefault(x=>x.Type=="azp");
+                            if(azpClaim is null || azpClaim.Value != builder.Configuration["ClerkSettings:AZP"])
+                            {
+                                context.Fail("AZP Claim is invalid or missing");
+                            }
+                            return Task.CompletedTask;
+                        },
+                        OnMessageReceived = context =>
+                        {
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("Cors", p =>
+                {
+                    p.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                });
+            });
 
             var app = builder.Build();
 
@@ -24,9 +67,10 @@ namespace Clerk.Auth
             }
 
             app.UseHttpsRedirection();
+            app.UseCors("Cors");
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
